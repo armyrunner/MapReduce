a7c005698c38f67807f69d97ba5582a74ee488c2
cade.gardner <d00299297@dmail.dixie.edu>
2020-04-24 15:46:06 -0600

    extra work

--- master.go
+++ master.go
@@ -1,11 +1,14 @@
 package main
 
+// package mapreduce
+
 // import (
 // 	"log"
 // 	"mapreduce"
 // 	"strconv"
 // 	"strings"
 // 	"unicode"
+// 	"flag"
 // )
 
 // func main() {
@@ -52,4 +55,36 @@ package main
 // 	p := mapreduce.Pair{Key: key, Value: strconv.Itoa(count)}
 // 	output <- p
 // 	return nil
+// }
+
+// // Start comment
+// func Start(client Interface) error{
+// 	var trueMaster bool
+// 	flag.BoolVar(&isMaster, "master", false, "Boolean indicated whether this master is true (default false = worker")
+// 	var port string
+// 	flag.StringVar(&port, "port", "8080", "Port to listen on")
+// 	var masterAddress string
+// 	flag.StringVar(&masterAddress, "MasterAddress", getLocalAddress()+":8080", "Address of master, ignore if it is master")
+// 	var split bool
+// 	split = true
+// 	var M, R int
+// 	flag.IntVar(&M, "m", 9, "Number of Map Tasks, ignore if it is the worker")
+// 	flag.IntVar(&R, "r", 3, "Number of Reduce Tasks, ignore if it is the worker")
+// 	var incoming string
+// 	flag.StringVar(&incoming, "Incoming file", "austen.sqlite3", "input database file, ignore if it is a worker")
+// 	flage.Parse()
+// 	var err error
+// 	if !trueMaster{
+// 		address := getAddress(port)
+// 		log.Printf("Worker Listening on address: %v", address)
+// 		log.Printf("Master located at address: %v", masterAddress)
+// 		err = worker(address, masterAddress, client)
+// 	} else {
+// 		address := getAddress(port)
+// 		log.Printf("Master Listening on address: %v", address)
+// 		err = master(address, infile, M, R, doSplit)
+// 	}
+// 	log.Printf("Goodbye!")
+// 	return err
+// 	}
 // }
